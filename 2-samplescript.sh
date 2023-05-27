e81b8c0c270c93f7dcb429abac070f0380efacc4
pzite <zi@gmail.com>
2023-05-27 06:48:05 +0530

    scripts for frontend

--- 2-samplescript.sh
+++ 2-samplescript.sh
@@ -1,3 +1,17 @@
-# this is my shell script no 2
-echo my name is a good one
 
+yum install nginx -y
+
+systemctl enable nginx
+systemctl start nginx
+
+rm -rf /usr/share/nginx/html/*
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+cd /usr/share/nginx/html
+
+unzip /tmp/frontend.zip
+
+#vim /etc/nginx/default.d/roboshop.conf
+
+systemctl restart nginx
